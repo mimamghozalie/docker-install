329576ee4654fd7134d6bf3467a925af6d3edb89
Mochamad Imam Ghozalie <xfust@yahoo.co.id>
2022-02-15 19:02:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+# install docker
+apt update
 
-# download
+apt install docker.io -y
+
+
+# install docker compose v2
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 
